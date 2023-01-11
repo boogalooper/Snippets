9df9f0ff3ee7394a595185e8b3d0aab305d3411a
boogalooper <jazzy.tver@gmail.com>
2023-01-11 23:53:11 +0300

    some updates

---
+++ @/@getDesc modern.jsx
@@ -0,0 +1,6 @@
+﻿#target photoshop
+s2t = stringIDToTypeID;
+(r = new ActionReference());//.putProperty(s2t('property'), p = s2t('json'));
+r.putEnumerated(s2t('layer'), s2t('ordinal'), s2t('targetEnum'));
+(d = new ActionDescriptor()).putObject(s2t('object'), s2t('object'), executeActionGet(r));
+$.writeln(executeAction(s2t('convertJSONdescriptor'), d).getString(s2t('json')));
---
+++ @/@getJSON.jsx
@@ -0,0 +1,11 @@
+﻿#target photoshop
+s2t = stringIDToTypeID;
+
+(r = new ActionReference()).putProperty(s2t("property"), s2t("json"));
+r.putEnumerated(s2t("document"), s2t("ordinal"), s2t("targetEnum"));
+(d = new ActionDescriptor()).putReference(s2t("null"), r);
+d.putBoolean(s2t("expandSmartObjects"), true);
+eval("var json=" + executeAction(s2t("get"), d, DialogModes.NO).getString(s2t("json")));
+
+$.writeln(json.toSource());
+
